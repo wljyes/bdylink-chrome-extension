ad797377b88ac56d4e3eb090eb12731e5a9fe70f
wljyes <1757280079@qq.com>
2020-01-19 21:50:25 +0800

    complete regex and test

--- background.js
+++ background.js
@@ -3,6 +3,20 @@ chrome.contextMenus.create({
     contexts:['selection'], //选中文字时才会出现此右键菜单
     onclick: function(params)
     {
-        
+        var reg = /([0-9a-zA-Z?=&]+)(?:[^0-9a-zA-Z]+)?([a-zA-Z]{4})?/;
+        var matches = reg.exec(params.selectionText);
+        if (matches.length == 0) {
+            //regex not match any string, maybe the selected string isn't a baidu pan link
+            //do nothing
+        } else if (matches.length == 2) {
+            //this means the link is without a verify code
+            var link_suffix = matches[1];
+            //just jump to the pan.baidu.com/s/$link_suffix
+            chrome.tabs.create({url:'https://pan.baidu.com/s/' + link_suffix});
+        } else if (matches.length == 3) {
+            //this means the link has a verify code
+            chrome.tabs.create({url:'https://pan.baidu.com/s/' + matches[1]});
+            document.getElementById("wkwj9A").innerText = matches[2];
+        }
     }
 })
